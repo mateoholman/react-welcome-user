faac9daecb0a2538fffb82869d4def2f3db5130f
Matthew Holman <mateoholman@gmail.com>
2017-10-24 13:42:10 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
