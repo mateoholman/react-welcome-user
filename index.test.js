cfc185550855594e8ba636724e348d5a23ac0549
Matthew Holman <mateoholman@gmail.com>
2017-10-23 22:38:04 -0500

    Finished adding test coverage

--- index.test.js
+++ index.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import Welcome from './index';
 
 describe('Welcome', () => {
   describe('when rendered', () => {
     it('should not throw an error', () => {
-      expect(shallow(<Welcome />).length).toBe(1);
+      const wrapper = shallow(<Welcome />);
+      expect(wrapper.length).toBe(1);
     });
 
     it('should render "Welcome User!" when no props are passed', () => {
-      expect(
-        shallow(<Welcome />)
-          .children()
-          .text()
-      ).toBe('Welcome User!');
+      expect(render(<Welcome />).text()).toBe('Welcome User!');
+    });
+
+    it('should render "Welcome Matt!" when "Matt" is passed as a user prop', () => {
+      expect(render(<Welcome user={'Matt'} />).text()).toBe('Welcome Matt!');
     });
   });
 });
